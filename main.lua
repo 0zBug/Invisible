c671eedfd3784443aed8e3d611e2cbb0ed723ed8
Bug <77031032+0zBug@users.noreply.github.com>
2022-08-11 20:14:26 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,4 +18,28 @@ function Invisible.Decode(Encoded)
     return string.gsub(Encoded, "....", Decode)
 end
 
+function Invisible.Encrypt(Original, Key)
+    local Encrypted = {}
+    local Key = {string.byte(Key, 1, -1)}
+    local Original = {string.byte(Original, 1, -1)}
+
+    for Index = 1, #Original do
+        table.insert(Encrypted, string.char(bit32.bxor(Original[Index], Key[Index % #Key + 1])))
+    end
+
+    return Invisible.Encode(table.concat(Encrypted))
+end
+
+function Invisible.Decrypt(Encrypted, Key)
+    local Decrypted = {}
+    local Key = {string.byte(Key, 1, -1)}
+    local Encrypted = {string.byte(Invisible.Decode(Encrypted), 1, -1)}
+
+    for Index = 1, #Encrypted do
+        table.insert(Decrypted, string.char(bit32.bxor(Encrypted[Index], Key[Index % #Key + 1])))
+    end
+
+    return table.concat(Decrypted)
+end
+
 return Invisible
